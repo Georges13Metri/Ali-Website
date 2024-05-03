0edfce3731c21117d692330bbdee70d0cfd6937a
GeorgesM13 <64842835+GeorgesM13@users.noreply.github.com>
2024-05-03 22:21:45 +0300

    h12

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
 import Hero from "@/components/Hero";
-import Navbar from "@/components/Navbar/Navbar";
-import { NavbarItems } from "@/components/Navbar/menuItems";
 
 export default function Home() {
   return (
     <div>
-      <Navbar items={NavbarItems} />
       <Hero />
     </div>
   );
