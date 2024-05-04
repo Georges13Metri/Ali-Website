5c2dc90dad864e404ab9ccb8f17bf6fd441b7a7e
GeorgesM13 <64842835+GeorgesM13@users.noreply.github.com>
2024-05-04 15:38:04 +0300

    hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,6 +6,7 @@ import React from "react";
 const Hero = () => {
   return (
     <div>
+      {/* Hero 1 */}
       <div className="relative h-screen">
         <div
           className="absolute inset-0 h-full bg-cover bg-center bg-no-repeat"
@@ -16,13 +17,13 @@ const Hero = () => {
         ></div>
         {/* Text content */}
         <div className="absolute top-1/2 left-0 transform -translate-y-1/2 px-8 max-w-xl">
-          <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold mb-2">
+          <h1 className="text-4xl text-black md:text-5xl lg:text-6xl font-bold mb-2">
             PERSONAL TRAINING
           </h1>
           <h1 className="text-2xl text-orange-700 font-semibold mb-4">
             IN LEBANON
           </h1>
-          <p className="text-lg md:text-xl lg:text-2xl mb-4">
+          <p className="text-black text-lg md:text-xl lg:text-2xl mb-4">
             Premium weight loss and lifestyle transformations which create long
             lasting, dramatic results to your health, body & mind.
           </p>
@@ -44,9 +45,10 @@ const Hero = () => {
           </div>
         </div>
       </div>
-      <div className=" flex justify-center bg-white items-center">
+      {/* Hero 2 */}
+      <div className=" flex justify-center border-2 border-red-600 bg-white items-center">
         <div className="flex mt-20 flex-col mx-auto">
-          <h2 className="text-3xl font-bold mb-10 text-center">
+          <h2 className="text-3xl font-bold mb-10 text-center text-black">
             WHY <span className="text-orange-700">ALI PERSONAL TRAINING</span>{" "}
             WILL WORK FOR YOU
           </h2>
@@ -54,7 +56,7 @@ const Hero = () => {
             A tonne of useful advice, guidance and coaching across the 4 keys to
             success that will help you:
           </p>
-          <ul className="flex-col justify-center items-center mx-auto">
+          <ul className="flex-col justify-center items-center mx-auto text-black">
             <li className="list-none">
               <span className="text-orange-700 font-bold">✓</span> Eat better,
               without dieting or feeling deprived.
@@ -76,7 +78,10 @@ const Hero = () => {
               maintain, your goals, even when life gets busy.
             </li>
           </ul>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mt-20 w-3/4 mx-auto px-5 py-5">
+          <div
+            className="grid grid-cols-1 md:grid-cols-2 gap-8 mt-20 w-3/4 
+          mx-auto px-5 py-5 text-black"
+          >
             <div>
               <Image
                 width={100}
@@ -146,130 +151,128 @@ const Hero = () => {
               </p>
             </div>
           </div>
-
-          <div className="mt-20 relative h-screen">
-            <div
-              className="absolute inset-0 h-full bg-cover bg-center bg-no-repeat"
-              style={{
-                backgroundImage: `url("/bg-2.jpg")`,
-                backgroundSize: "cover",
-              }}
-            ></div>
-            <div className="absolute inset-0 w-full md:w-1/2 mx-auto flex flex-col justify-center items-center text-center md:text-right px-8">
-              <h1 className="text-lg md:text-2xl lg:text-2xl font-serif text-white mb-6 md:mb-10">
-                ALI PERSONAL TRAINING CREDO
-              </h1>
-              <p className="text-white text-xs md:text-sm mb-6 md:mb-10">
-                <span className="font-bold">I help</span> busy men and women
-                overhaul their health & fitness using a combination of tailored
-                exercise, nutrition, lifestyle and mindset systems that are
-                holistic and sustainable forever{" "}
-                <span className="font-bold">so they can..</span>
-              </p>
-              <ul className="text-white text-xs md:text-sm mb-6 md:mb-10 text-left">
-                <li>
-                  <span>
-                    ✓ Feel confident and attractive around their friends,
-                    family, and out in public
-                  </span>
-                </li>
-                <li>
-                  <span>
-                    ✓ Feel Physically & mentally strong, capable of taking on
-                    any challenge without worrying that their energy levels or
-                    body weight will get in the way.
-                  </span>
-                </li>
-                <li>
-                  <span>✓ Fit into the clothes they want to wear</span>
-                </li>
-                <li>
-                  <span>
-                    ✓ Stop worrying about getting diseases and dying young
-                  </span>
-                </li>
-                <li>
-                  <span>
-                    ✓ Run around with their kids, or grandkids, without feeling
-                    pain, winded or tired; and do it again the next day
-                  </span>
-                </li>
-                <li>
-                  <span>
-                    ✓ Add 10+ years of healthy living to their retirement
-                  </span>
-                </li>
-              </ul>
-              <p className="text-white font-bold text-xs md:text-sm mb-5">
-                Without having to....
-              </p>
-              <ul className="text-white text-xs md:text-sm mb-6 md:mb-10 text-left">
-                <li>
-                  <span>✓ Starve themselves - No diets!</span>
-                </li>
-                <li>
-                  <span>✓ Count calories or weigh foods</span>
-                </li>
-                <li>
-                  <span>✓ Give up the foods they enjoy</span>
-                </li>
-                <li>
-                  <span>✓ Spend hours cooking or exercising</span>
-                </li>
-                <li>
-                  <span>✓ Share a gym with others</span>
-                </li>
-              </ul>
-              <Link href="/aboutus">
-                <button
-                  className="bg-orange-700 hover:bg-transparent text-white font-semibold py-2 px-4 transition duration-300 ease-in-out hover:border
-             hover:text-orange-700 hover:border-orange-700 border border-orange-700 "
-                >
-                  FIND OUT MORE
-                </button>
-              </Link>
-            </div>
-          </div>
-
-          <div
-            className="grid bg-white grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8 
-           p-4 md:p-8  items-center"
-          >
-            <div className="mx-auto">
-              <Image
-                width={200}
-                height={100}
-                src="/clientfree.png"
-                alt="freeclient"
-              />
-            </div>
-            <div className="flex flex-col justify-center mt-20">
-              <h1 className="text-lg md:text-3xl font-bold mb-4 font-serif">
-                A FREE TRANSFORMATION CONSULTATION (£50.00 VALUE)
-              </h1>
-              <p className="text-sm md:text-base font-serif mb-6 md:mb-8 ">
-                Take the first step to success by applying for a FREE
-                Transformation Consultation. We will talk about your needs, your
-                goals, lifestyle habits, your diet, and establish if we are a
-                good fit to work together. I will give you actionable advice on
-                how you can successfully progress and achieve more than you
-                thought possible. There are limited slots available as I only
-                work with a small number of clients at a time, so please
-                complete the form in as much detail as you can and I will be in
-                touch with you soon.
-              </p>
-              <Link href="/">
-                <button
-                  className="mb-20 hover:bg-gray-200 text-orange-700  py-2 px-4 md:px-8 transition duration-300
-                 ease-in-out hover:border hover:text-orange-700 hover:border-orange-700 border border-orange-700 text-sm md:text-base"
-                >
-                  CLAIM OFFER
-                </button>
-              </Link>
-            </div>
-          </div>
         </div>
       </div>
+      {/* Hero 3 */}
+      <div className="relative h-screen">
+        <div
+          className="absolute inset-0 h-full bg-cover bg-center bg-no-repeat"
+          style={{
+            backgroundImage: `url("/bg-2.jpg")`,
+            backgroundSize: "cover",
+          }}
+        ></div>
+        <div className="absolute inset-0 w-full md:w-1/2 mx-auto flex flex-col justify-center items-center text-center md:text-right px-8">
+          <h1 className="text-lg md:text-2xl lg:text-2xl font-serif text-white mb-6 md:mb-10">
+            ALI PERSONAL TRAINING CREDO
+          </h1>
+          <p className="text-white text-xs md:text-sm mb-6 md:mb-10">
+            <span className="font-bold">I help</span> busy men and women
+            overhaul their health & fitness using a combination of tailored
+            exercise, nutrition, lifestyle and mindset systems that are holistic
+            and sustainable forever{" "}
+            <span className="font-bold">so they can..</span>
+          </p>
+          <ul className="text-white text-xs md:text-sm mb-6 md:mb-10 text-left">
+            <li>
+              <span>
+                ✓ Feel confident and attractive around their friends, family,
+                and out in public
+              </span>
+            </li>
+            <li>
+              <span>
+                ✓ Feel Physically & mentally strong, capable of taking on any
+                challenge without worrying that their energy levels or body
+                weight will get in the way.
+              </span>
+            </li>
+            <li>
+              <span>✓ Fit into the clothes they want to wear</span>
+            </li>
+            <li>
+              <span>
+                ✓ Stop worrying about getting diseases and dying young
+              </span>
+            </li>
+            <li>
+              <span>
+                ✓ Run around with their kids, or grandkids, without feeling
+                pain, winded or tired; and do it again the next day
+              </span>
+            </li>
+            <li>
+              <span>✓ Add 10+ years of healthy living to their retirement</span>
+            </li>
+          </ul>
+          <p className="text-white font-bold text-xs md:text-sm mb-5">
+            Without having to....
+          </p>
+          <ul className="text-white text-xs md:text-sm mb-6 md:mb-10 text-left">
+            <li>
+              <span>✓ Starve themselves - No diets!</span>
+            </li>
+            <li>
+              <span>✓ Count calories or weigh foods</span>
+            </li>
+            <li>
+              <span>✓ Give up the foods they enjoy</span>
+            </li>
+            <li>
+              <span>✓ Spend hours cooking or exercising</span>
+            </li>
+            <li>
+              <span>✓ Share a gym with others</span>
+            </li>
+          </ul>
+          <Link href="/aboutus">
+            <button
+              className="bg-orange-700 hover:bg-transparent text-white font-semibold py-2 px-4 transition duration-300 ease-in-out hover:border
+             hover:text-orange-700 hover:border-orange-700 border border-orange-700 "
+            >
+              FIND OUT MORE
+            </button>
+          </Link>
+        </div>
+      </div>
+      {/* Hero 4 */}
+      <div
+        className="grid bg-white grid-cols-1 md:grid-cols-2 
+      lg:grid-cols-2 gap-8 p-4 md:p-8  items-center"
+      >
+        <div className="mx-auto">
+          <Image
+            width={200}
+            height={100}
+            src="/clientfree.png"
+            alt="freeclient"
+          />
+        </div>
+        <div className="flex flex-col justify-center mt-20 text-black">
+          <h1 className="text-lg md:text-3xl font-bold mb-4 font-serif">
+            A FREE TRANSFORMATION CONSULTATION (£50.00 VALUE)
+          </h1>
+          <p className="text-sm md:text-base font-serif mb-6 md:mb-8 ">
+            Take the first step to success by applying for a FREE Transformation
+            Consultation. We will talk about your needs, your goals, lifestyle
+            habits, your diet, and establish if we are a good fit to work
+            together. I will give you actionable advice on how you can
+            successfully progress and achieve more than you thought possible.
+            There are limited slots available as I only work with a small number
+            of clients at a time, so please complete the form in as much detail
+            as you can and I will be in touch with you soon.
+          </p>
+          <Link href="/">
+            <button
+              className="mb-20 hover:bg-gray-200 text-orange-700  py-2 px-4 md:px-8 transition duration-300
+                 ease-in-out hover:border hover:text-orange-700 hover:border-orange-700 border border-orange-700 text-sm md:text-base"
+            >
+              CLAIM OFFER
+            </button>
+          </Link>
+        </div>
+      </div>
+      {/* Hero 5 */}
       <div className="relative h-screen">
         <div
           className="absolute inset-0 h-full bg-cover bg-center bg-no-repeat"
@@ -388,6 +391,7 @@ const Hero = () => {
           </div>
         </div>
       </div>
+      {/* Hero 6 */}
       <div className="bg-gray-300 pt-10 pb-5">
         <div className="container mx-auto">
           <div
@@ -436,16 +440,20 @@ const Hero = () => {
           </div>
         </div>
       </div>
+      {/* Hero 7 */}
       <div
         className="flex flex-col md:flex-row 
       items-center justify-center md:justify-between
-       px-4 py-8 md:py-12 bg-whit mb-10"
+       px-4 py-8 md:py-12 bg-whit mb-10 bg-white"
       >
-        <div className="md:w-1/2 mb-4 md:mb-0 font-serif">
-          <h2 className="text-3xl md:text-4xl font-bold text-center md:text-left mb-4">
+        <div className="md:w-1/2 mb-4 md:mb-0 font-serif ">
+          <h2
+            className="text-3xl md:text-4xl font-bold text-black
+           text-center md:text-left mb-4"
+          >
             PERSONAL TRAINING IN LANCING
           </h2>
-          <p className="text-lg text-gray-700 leading-relaxed text-center md:text-left">
+          <p className="text-lg text-gray-800 leading-relaxed text-center md:text-left">
             At Ali Personal Training in Lancing I &apos; m so passionate and
             confident about achieving results for my clients that if you commit
             and do everything within my coaching program and don &apos; t see
